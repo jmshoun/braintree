bfd00522d010a64a9574b14aad2dcbfac33660b0
Mark Shoun <mark.shoun@gmail.com>
2017-07-01 21:23:27 -0400

    Cache preprocessed data files
    Converting the million-song CSV into a numpy matrix takes upwards of a
    minute on my laptop, so it made sense to split off the preprocessing
    step into a seperate script and pickle the data. This considerably
    speeds up the primary fitting script.

--- fit.py
+++ fit.py
@@ -1,9 +1,8 @@
 import warnings
 import re
 import os
+import pickle
 
-import sklearn.model_selection
-import sklearn.preprocessing
 import numpy as np
 
 import braintree
@@ -13,34 +12,6 @@ with warnings.catch_warnings():
     import xgboost as xgb
 
 
-def load_data(filename, response_column=0):
-    """Loads a CSV data set and returns NumPy train, validation and test data sets."""
-    data = np.genfromtxt(filename, delimiter=",")
-    y = data[:, response_column, None]
-    X = np.delete(data, response_column, axis=1)
-    X_train, X_nontrain, y_train, y_nontrain = sklearn.model_selection.train_test_split(
-        X, y, test_size=0.4, random_state=1729)
-    X_validation, X_test, y_validation, y_test = sklearn.model_selection.train_test_split(
-        X_nontrain, y_nontrain, test_size=0.4, random_state=1729)
-    return {"train": {"X": X_train, "y": y_train},
-            "validation": {"X": X_validation, "y": y_validation},
-            "test": {"X": X_test, "y": y_test}}
-
-
-def standardize_data(data):
-    """Standardizes the predictors and response to have zero mean and unit variance."""
-    x_standardizer = sklearn.preprocessing.StandardScaler()
-    x_standardizer.fit(data["train"]["X"])
-    y_standardizer = sklearn.preprocessing.StandardScaler()
-    y_standardizer.fit(data["train"]["y"])
-
-    def standardize(data_set):
-        return {"X": x_standardizer.transform(data_set["X"]),
-                "y": y_standardizer.transform(data_set["y"])}
-
-    return {k: standardize(v) for k, v in data.items()}
-
-
 def convert_to_dmatrix(data):
     return xgb.DMatrix(data["X"], label=data["y"])
 
@@ -48,11 +19,11 @@ def convert_to_dmatrix(data):
 def fit_gbm(data):
     """Fits a GBM to training data."""
     xgb_data = {k: convert_to_dmatrix(v) for k, v in data.items()}
-    return xgb.train({"eta": 0.5, "max_depth": 2},
+    return xgb.train({"eta": 0.2, "max_depth": 6, "subsample": 0.5},
                       dtrain=xgb_data["train"],
-                      num_boost_round=20,
+                      num_boost_round=500,
                       evals=[(xgb_data["train"], "train"), (xgb_data["validation"], "validation")],
-                      verbose_eval=False)
+                      verbose_eval=True)
 
 
 def gbm_to_params(model, num_predictors):
@@ -117,8 +88,8 @@ def parse_tree(tree, tree_number, max_depth, params):
     return params
 
 
-songs = load_data("data/YearPredictionMSD.txt")
-songs = standardize_data(songs)
+with open("data/song_matrix.p", "rb") as infile:
+    songs = pickle.load(infile)
 num_predictors = songs["train"]["X"].shape[1]
 
 train_data = braintree.TensorFlowData(songs["train"]["X"], songs["train"]["y"])
