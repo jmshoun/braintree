249b4258c4cd3588eab28d09b3076acd50a2b242
Mark Shoun <mark.shoun@gmail.com>
2018-01-15 23:06:48 -0500

    Load testing data sets in context

--- tests/context.py
+++ tests/context.py
@@ -1,5 +1,12 @@
 import os
 import sys
+
+import numpy as np
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import braintree.data as data
+import braintree.xgboost as xgboost
+
+# Sample data sets for testing
+concrete = np.loadtxt("tests/test_data/concrete.csv", delimiter=",", skiprows=1)
