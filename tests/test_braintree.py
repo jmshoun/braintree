d35f08b1f5cfc4a7b377b0198c501ef2f065443d
Mark Shoun <mark.shoun@gmail.com>
2018-02-20 21:10:49 -0500

    Add tests of model scoring

--- tests/test_braintree.py
+++ tests/test_braintree.py
@@ -15,3 +15,11 @@ class BrainTreeTest(unittest.TestCase):
     def test_model_construction(self):
         model = braintree.BrainTree(num_trees=25)
         self.assertIsNotNone(model)
+
+    def test_model_scoring(self):
+        model = braintree.BrainTree(num_trees=15)
+        with self.assertRaises(braintree.NotFitError):
+            model.predict(concrete_test)
+        model.fit(concrete_train, concrete_test)
+        predictions = model.predict(concrete_test)
+        self.assertEqual(len(predictions), 31)
