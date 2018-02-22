93f02d8cbe3a7c38ce4c6a7acfb912ba1326cf3d
Mark Shoun <mark.shoun@gmail.com>
2018-02-21 22:14:13 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="braintree",
+      version="0.1",
+      description="Braintree statistical models",
+      author="Mark Shoun",
+      author_email="mark.shoun@gmail.com",
+      packages=["braintree"])
