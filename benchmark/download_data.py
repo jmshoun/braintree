790e2e5e2ec14251ac7de54a0cb8f902467ebf63
Mark Shoun <mark.shoun@gmail.com>
2018-03-25 14:50:29 -0400

    Unzip and split compressed datasets

--- benchmark/download_data.py
+++ benchmark/download_data.py
@@ -1,20 +1,87 @@
 #!/usr/bin/env python
 
-import shutil
+import re
+import zipfile
 import os
+import glob
+import json
 
 import requests
+import progressbar
 
 
-def download_url(url, filename):
-    print("Downloading file from {}...".format(url))
-    response = requests.get(url, stream=True)
-    with open(filename, "wb") as outfile:
-        shutil.copyfileobj(response.raw, outfile)
+def download_dataset(meta):
+    print("Downloading file from {}...".format(meta["url"]))
+    response = requests.get(meta["url"], stream=True)
+    file_size = int(response.headers.get("content-length"))
+    file_name = "benchmark/data/{name}.{extension}".format(**meta)
+    with open(file_name, "wb") as outfile:
+        downloaded = 0
+        bar = progressbar.ProgressBar(max_value=file_size)
+        for block in response.iter_content(chunk_size=4096):
+            downloaded += len(block)
+            outfile.write(block)
+            bar.update(downloaded)
+    if meta["extension"] == "zip":
+        _unzip(meta)
+        os.remove(file_name)
+    if "train_size" in meta:
+        _split(meta)
 
 
-if not os.path.exists("benchmark/data"):
-    os.mkdir("benchmark/data")
+def _split(meta):
+    name = meta["name"]
+    main = f"benchmark/data/{name}.csv"
+    train = f"benchmark/data/{name}-train.csv"
+    test = f"benchmark/data/{name}-test.csv"
+    with open(main, "r") as main_file:
+        if meta["header"]:
+            main_file.readline()
+        with open(train, "w") as train_file:
+            for _ in range(meta["train_size"]):
+                train_file.write(main_file.readline())
+        with open(test, "w") as test_file:
+            for line in main_file:
+                test_file.write(line)
+    os.remove(main)
 
-download_url("http://archive.ics.uci.edu/ml/machine-learning-databases/00265/CASP.csv",
-             "benchmark/data/CASP.csv")
+
+def _match_names(pattern, members):
+    pattern_clean = pattern.replace(".", r"\.").replace("*", ".*")
+    return [member for member in members if re.match(pattern_clean, member)]
+
+
+def _unzip(meta):
+    name = meta["name"]
+    filenames = meta["filenames"]
+    with zipfile.ZipFile(f"benchmark/data/{name}.zip") as infile:
+        if "all" in filenames:
+            names = _match_names(filenames["all"], infile.namelist())
+            _unzip_files(infile, names, f"benchmark/data/{name}.csv", meta["header"])
+        else:
+            train_names = _match_names(filenames["train"], infile.namelist())
+            _unzip_files(infile, train_names, f"benchmark/data/{name}-train.csv", meta["header"])
+            test_names = _match_names(filenames["test"], infile.namelist())
+            _unzip_files(infile, test_names, f"benchmark/data/{name}-test.csv", meta["header"])
+
+
+def _unzip_files(zip_file, names, dest, header=False):
+    with open(dest, "wb") as dest_file:
+        first_name = True
+        for name in names:
+            with zip_file.open(name, "r") as source_file:
+                if header:
+                    if first_name:
+                        first_name = False
+                    else:
+                        source_file.readline()
+                for line in source_file.readlines():
+                    dest_file.write(line)
+
+
+if __name__ == "__main__":
+    meta_files = glob.glob("benchmark/data/*-meta.json")
+    for filename in meta_files:
+        with open(filename, "r") as file:
+            metadata = json.load(file)
+            download_dataset(metadata)
